494d6ab3bfb127a225ac3813c01574afb5f7c971
Felipe <machadofelipe2016@outlook.com>
2022-03-25 19:30:28 -0300

    c5- survey about error about deleted

--- mymoney/src/rest.js
+++ mymoney/src/rest.js
@@ -1,6 +1,12 @@
 import {useEffect, useReducer} from 'react'
 import axios from 'axios'
 
+const INITIAL_STATE = {
+    loading: false,
+    data: {}
+}
+
+
 const reducer  = (state, action) =>{
   
   if(action.type ==='REQUEST'){
@@ -19,22 +25,73 @@ const reducer  = (state, action) =>{
     return state
 }
 
-const baseUrl = 'https://monthly-app-2d69d-default-rtdb.firebaseio.com/'
-const useGet =  resource =>{
-    const [data, dispatch] = useReducer(reducer, {loading:true, data:{}})
-    useEffect(() => {
-      dispatch({type:'REQUEST'})
-      axios
-      .get(baseUrl + resource + '.json')
-      .then(res => {
-        console.log(res.data)
-        dispatch({type:'SUCCESS', data: res.data})
-      })
-    },[])
-     return data
+// this called inject dependence!!
+const init = baseUrl => {
+    const useGet =  resource =>{
+        const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
+        useEffect(() => {
+          dispatch({type:'REQUEST'})
+          axios
+          .get(baseUrl + resource + '.json')
+          .then(res => {
+            console.log(res.data)
+            dispatch({type:'SUCCESS', data: res.data})
+          })
+        },[])
+         return data
+    }
+
+    const usePost = resource =>{
+        const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
+        const post = data =>{
+            dispatch({type:'REQUEST'})
+            axios
+            .post(baseUrl+ resource+ '.json', data)
+            .then(res =>{ 
+                dispatch({type:'SUCCESS', data:res.data
+            })
+        }) 
+       }
+         return [data,post]
+    }
+    
+
+    
+    const useDelete = () =>{     
+    const [data, dispatch] = useReducer(reducer, INITIAL_STATE)
+     //this is useffect turn of function remove
+    const remove = resource => {
+        dispatch({type: 'REQUEST'})    
+        axios
+        .delete(baseUrl + resource + 'json')
+        .then(() => {
+            dispatch({type:'SUCCESS'
+          })
+        })
+        }
+      return [data, remove]
+    }
+
+
+     
+    
+    
+    
+    return{
+         useGet,
+         usePost,
+         useDelete
+     }
+
+
+
+
 }
 
-export default useGet
+
+
+
+export default init
 
 
 
