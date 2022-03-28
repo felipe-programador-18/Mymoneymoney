01ad9c8372a21450fcdfc233b1b25f83cea0650a
Felipe <machadofelipe2016@outlook.com>
2022-03-28 17:50:32 -0300

    c9-adding new date and create new projetc

--- mymoney/src/App.js
+++ mymoney/src/App.js
@@ -7,40 +7,61 @@ import Rest from './rest'
 
 const baseUrl = 'https://monthly-app-2d69d-default-rtdb.firebaseio.com/'
 
-const {useGet,usePost, useDelete } = Rest(baseUrl)
+const {useGet} = Rest(baseUrl)
 
 
 function App() {
-  const data = useGet('movimentacoes')
-  const [postData, post] = usePost('movimentacoes')
-  const [postRemove, remove] = useDelete()
+ // const data = useGet('movimentacoes/2022-03')
+  const data = useGet('meses')
+  //const [postData, post] = usePost('movimentacoes/2022-03')
+  //const [postRemove, remove] = useDelete()
 
-   const Save =  () =>{
-    post({
-      valor:'10', descricao:'remerber practice about that'
-    })
-    }
+   //const Save =  () =>{
+   // post({
+    //  valor:'10000', descricao:'remerber always practice'
+   // })
+   // }
 
-    const Removed = () =>{
-      remove('movimentacoes.json' )
-    }
+    //const Removed = () =>{
+      //remove('movimentacoes/2022-03/-MzGw_EcpVOHagYECpIW' )
+    //}
 
 
   return (
+
     <div className="App">
-    <h1> My Money </h1>
-     {JSON.stringify(data)}
-    {data.loading && <p> loading ....</p>  } 
 
-       <button onClick={Save} >Salvar</button>
-       <button onClick={Removed}>Deleta</button> 
-    
-    <pre> {JSON.stringify(postRemove)}  </pre>
-    <pre> {JSON.stringify(postData)}  </pre>
+    <nav className='navbar navbar-dark bg-primary '>
+      <div className='container'>
+         <a href='/' className=' navbar-brand '>My Money</a>
+      </div>
+    </nav>
 
-   
+    <h2>Adicionar mês</h2>
+      <select>
+        <option value='2019'>2019</option>
+        <option value='2020'>2020</option>
+      </select>
 
-   <h1 className='text-danger'>teste bootstrap</h1>
+        <select>
+        <option value='01'>01</option>
+        <option value='02'>02</option>
+      </select>   
+
+      <button>Adicionar mês</button>
+
+      {JSON.stringify(data)}
+          
+       { data.loading && <span>Carregando...</span> }
+
+       { !data.loading && <table className='table'>
+           <tbody>
+             <tr>
+               <td> 2019-09 </td>
+               <td>0</td>
+             </tr>
+           </tbody>
+       </table> }
    
 
     </div>
