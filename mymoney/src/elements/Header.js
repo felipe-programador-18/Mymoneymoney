e80a7f3058742a189aa4ffe85cce038f10c1f0e0
Felipe <machadofelipe2016@outlook.com>
2022-03-30 17:05:13 -0300

    c14- adding new folder elements about header

---
+++ mymoney/src/elements/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const Header = () => {
+   return(
+    <nav className='navbar navbar-dark bg-primary '>
+      <div className='container'>
+         <a href='/' className=' navbar-brand '>My Money</a>
+      </div>
+    </nav>
+    )
+}
+
+
+export default Header
