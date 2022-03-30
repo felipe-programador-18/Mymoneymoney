3ac9d6b8f477c7298a331e2a072e3bb8770aa53f
Felipe <machadofelipe2016@outlook.com>
2022-03-30 17:04:46 -0300

    c13- adding new date about page principles

--- mymoney/src/App.js
+++ mymoney/src/App.js
@@ -3,68 +3,23 @@ import React from 'react'
 //import useGet from './useGet'
 //import usePost from './usePost'
 //import useDelete from './useDelete'
-import Rest from './rest'
-
-const baseUrl = 'https://monthly-app-2d69d-default-rtdb.firebaseio.com/'
-
-const {useGet} = Rest(baseUrl)
+import Header from './elements/Header'
+import Meses from './Meses'
+import Adicionarmes from './Adicionar.meses'
 
 
 function App() {
- // const data = useGet('movimentacoes/2022-03')
-  const data = useGet('meses')
-  //const [postData, post] = usePost('movimentacoes/2022-03')
-  //const [postRemove, remove] = useDelete()
-
-   //const Save =  () =>{
-   // post({
-    //  valor:'10000', descricao:'remerber always practice'
-   // })
-   // }
-
-    //const Removed = () =>{
-      //remove('movimentacoes/2022-03/-MzGw_EcpVOHagYECpIW' )
-    //}
-
-
   return (
-
-    <div className="App">
-
-    <nav className='navbar navbar-dark bg-primary '>
-      <div className='container'>
-         <a href='/' className=' navbar-brand '>My Money</a>
-      </div>
-    </nav>
-
-    <h2>Adicionar mês</h2>
-      <select>
-        <option value='2019'>2019</option>
-        <option value='2020'>2020</option>
-      </select>
-
-        <select>
-        <option value='01'>01</option>
-        <option value='02'>02</option>
-      </select>   
-
-      <button>Adicionar mês</button>
-
-      {JSON.stringify(data)}
-          
-       { data.loading && <span>Carregando...</span> }
-
-       { !data.loading && <table className='table'>
-           <tbody>
-             <tr>
-               <td> 2019-09 </td>
-               <td>0</td>
-             </tr>
-           </tbody>
-       </table> }
+ <div className="App">
+    <Header/>
+    <Adicionarmes/>
+  <div className='container'>
    
-
-    </div>
+     <Meses/>
+       
+       
+  </div>  
+</div>
   );
 }
 
