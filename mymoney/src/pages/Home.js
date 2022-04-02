1012effa687f9117c026b9b6afed113b138522d0
Felipe <machadofelipe2016@outlook.com>
2022-04-02 11:07:43 -0300

    c17- create folder pages to rearrange all code

---
+++ mymoney/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Meses from '../Meses'
+import Adicionarmes from '../Adicionar.meses'
+const Home = () =>{
+    return(
+      <div className='container'>
+      <Adicionarmes/>
+       <Meses/>
+       </div>  
+    )
+  }
+  
+export default Home
---
+++ mymoney/src/pages/Movimentacoes.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Movimentacoes = () =>{
+    return(
+      <h1 className='text-danger'>Movimentações</h1>
+    )
+  }
+
+export default Movimentacoes
+
+
+
+
+
+
