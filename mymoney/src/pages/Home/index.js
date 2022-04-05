581db8c1a69979ea35852eb8349d86cd504b2841
Felipe <machadofelipe2016@outlook.com>
2022-04-05 16:08:07 -0300

    c27- adding and built new folder pages

--- mymoney/src/pages/Home/index.js
+++ mymoney/src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Meses from '../../Meses'
-import Adicionarmes from '../../Adicionar.meses'
+import Meses from './Meses'
+import Adicionarmes from './Adicionar.meses'
 const Home = () =>{
     return(
       <div className='container'>
